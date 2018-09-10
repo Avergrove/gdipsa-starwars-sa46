053ccfcb7ab36cfd6b8083f72cee2e54b82ab10a
Chua Khiong Yang  (NCS) <khiongyang.chua@ncs.com.sg>
2018-09-10 15:26:36 +0800

    Added models for each category of items, adjusted json item mapping

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { PeopleMeta, FilmMeta , SpeciesMeta, StarshipMeta, VehicleMeta, PlanetMeta} from './model'
 
 @Component({
   selector: 'app-root',
@@ -16,65 +17,65 @@ export class AppComponent {
 
   loadCategory($event) {
     this.category = $event;
+    this.items = [];
 
     switch ($event) {
       case "Characters":
-        this.http.get('https://swapi.co/api/people/').subscribe((data) => {
+        this.http.get<PeopleMeta>('https://swapi.co/api/people/').subscribe((data) => {
           data.results.forEach((element, index) => {
-            this.items.push(element.name);
+            this.items.push(element);
           });
         })
         break;
 
       case "Films":
-        this.http.get('https://swapi.co/api/films/').subscribe((data) => {
+        this.http.get<FilmMeta>('https://swapi.co/api/films/').subscribe((data) => {
           data.results.forEach((element, index) => {
-            this.items.push(element.title);
+            this.items.push(element);
           });
         })
         break;
 
 
       case "Species":
-        this.http.get('https://swapi.co/api/species/').subscribe((data) => {
+        this.http.get<SpeciesMeta>('https://swapi.co/api/species/').subscribe((data) => {
           data.results.forEach((element, index) => {
-            this.items.push(element.name);
+            this.items.push(element);
           });
         })
         break;
 
 
       case "Starships":
-        this.http.get('https://swapi.co/api/starships/').subscribe((data) => {
+        this.http.get<StarshipMeta>('https://swapi.co/api/starships/').subscribe((data) => {
           data.results.forEach((element, index) => {
-            this.items.push(element.name);
+            this.items.push(element);
           });
         })
         break;
 
 
       case "Vehicles":
-        this.http.get('https://swapi.co/api/vehicles/').subscribe((data) => {
+        this.http.get<VehicleMeta>('https://swapi.co/api/vehicles/').subscribe((data) => {
           data.results.forEach((element, index) => {
-            this.items.push(element.name);
+            this.items.push(element);
           });
         })
         break;
 
 
       case "Planets":
-        this.http.get('https://swapi.co/api/planets/').subscribe((data) => {
+        this.http.get<PlanetMeta>('https://swapi.co/api/planets/').subscribe((data) => {
           data.results.forEach((element, index) => {
-            this.items.push(element.name);
+            this.items.push(element);
           });
         })
         break;
 
-
-
       default:
         break;
     }
+
   }
 
 }
--- src/app/model.ts
+++ src/app/model.ts
@@ -1,3 +1,161 @@
-export interface Category{
+export interface PeopleMeta {
+    count: string;
+    next: string;
+    previous: string;
+    results: People[];
+}
+
+export interface People {
     name: string;
+    height: string;
+    mass: number;
+    hair_color: string;
+    skin_color: string;
+    birth_year: string;
+    gender: string;
+
+    homeworld: string; // Url
+    films: string[];   // Url
+    species: string[]; // Url
+    vehicles: string[];
+    starships: string[];
+
+    created: string[];
+    edited: string[];
+
+    url: string;
+}
+
+
+
+export interface FilmMeta {
+    count: string;
+    next: string;
+    previous: string;
+    results: Film[];
+}
+
+export interface Film {
+    title: string;
+    episode_id: string;
+    opening_crawl: string;
+    director: string;
+    producer: string;
+    release_date: string;
+    characters: string[];
+    planets: string[];
+    starships: string[];
+    vehicles: string[];
+    species: string[];
+    created: string;
+    edited: string;
+    url: string;
+}
+
+
+
+export interface SpeciesMeta {
+    count: string;
+    next: string;
+    previous: string;
+    results: Species[];
+}
+
+export interface Species {
+    name: string;
+    classification: string;
+    designation: string;
+    average_height: string;
+    skin_colors: string;
+    hair_colors: string;
+    eye_colors: string;
+    average_lifespan: string;
+    homeworld: string;
+    language: string;
+
+    people: string[];
+    films: string[];
+
+    created: string;
+    edited: string;
+    url: string;
+}
+
+export interface StarshipMeta {
+    count: string;
+    next: string;
+    previous: string;
+    results: Starship[];
+}
+
+export interface Starship{
+    name: string;
+    model: string;
+    manufacturer: string;
+    cost_in_credits: string;
+    length: string;
+    max_atmosphering_speed: string;
+    crew: string;
+    passengers: string;
+    cargo_capacity: string;
+    consumables: string;
+    hyperdrive_rating: string;
+    MGLT: string;
+    starship_class: string;
+    pilots: string[];
+    films: string[];
+
+    created: string;
+    edited: string;
+    url: string;
+}
+
+export interface VehicleMeta {
+    count: string;
+    next: string;
+    previous: string;
+    results: Vehicle[];
+}
+
+export interface Vehicle{
+    name: string;
+    model: string;
+    manufacturer: string;
+    cost_in_credits: string;
+    length: string;
+    max_atmosphering_speed: string;
+    crew: string;
+    passengers: string;
+    cargo_capacity: string;
+    consumables: string;
+    vehicle_class: string;
+    pilots: string[];
+    films: string[];
+    created: string;
+    edited: string;
+    url: string;
+}
+
+export interface PlanetMeta{
+    count: string;
+    next: string;
+    previous: string;
+    results: Planet[];
+}
+
+export interface Planet{
+    name: string;
+    rotation_period: string;
+    orbital_period: string;
+    diameter: string;
+    climate: string;
+    gravity: string;
+    terrain: string;
+    surface_water: string;
+    population: string;
+    residents: string[];
+    films: string[];
+    created: string;
+    edited: string;
+    url: string;
 }
