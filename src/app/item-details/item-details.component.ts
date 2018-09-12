48b5843948d726961bc1983911a1371f8125282f
Avergrove <khiongyang.chua@ncs.com.sg>
2018-09-12 16:03:34 +0800

    Improved tweet button, added better UI to category items

--- src/app/item-details/item-details.component.ts
+++ src/app/item-details/item-details.component.ts
@@ -29,32 +29,32 @@ export class ItemDetailsComponent implements OnInit {
     switch (this.item.category) {
       case 'people':
         this.imageUrl = "https://starwars-visualguide.com/assets/img/characters/" + itemId + ".jpg";
-        this.tweetText = encodeURI(this.tweetText + "\n" + this.item.name + " is my favourite boi");
+        this.tweetText = encodeURI(this.tweetText + "\n" + this.item.name + " is my favourite boi!");
         break
 
       case 'film':
         this.imageUrl = "https://starwars-visualguide.com/assets/img/films/" + itemId + ".jpg";
-        this.tweetText = encodeURI(this.tweetText + "\n" + this.item.title + " is my favourite movie.");
+        this.tweetText = encodeURI(this.tweetText + "\n" + this.item.title + " is my favourite movie!");
         break;
 
       case 'species':
         this.imageUrl = "https://starwars-visualguide.com/assets/img/species/" + itemId + ".jpg";
-        this.tweetText = encodeURI(this.tweetText + "\n" + "Me-sah likey" + this.item.name);
+        this.tweetText = encodeURI(this.tweetText + "\n" + "Me-sah likey" + this.item.name + "!");
         break;
 
       case 'starship':
         this.imageUrl = "https://starwars-visualguide.com/assets/img/starships/" + itemId + ".jpg";
-        this.tweetText = encodeURI(this.tweetText + "\n" + this.item.name + " is my favourite starsheep");
+        this.tweetText = encodeURI(this.tweetText + "\n" + this.item.name + " is my favourite starsheep!");
         break;
 
       case 'vehicle':
         this.imageUrl = "https://starwars-visualguide.com/assets/img/vehicles/" + itemId + ".jpg";
-        this.tweetText = encodeURI(this.tweetText + "\n" + this.item.name + " is my favourite HOOOOOOOOOOO");
+        this.tweetText = encodeURI(this.tweetText + "\n" + this.item.name + " is my favourite HOOOOOOOOOOO!");
         break;
 
       case 'planet':
         this.imageUrl = "https://starwars-visualguide.com/assets/img/planets/" + itemId + ".jpg";
-        this.tweetText = encodeURI(this.tweetText + "\n" + this.item.name + " is my favourite planet that totally didn't get obliterated");
+        this.tweetText = encodeURI(this.tweetText + "\n" + this.item.name + " is my favourite planet that totally didn't get obliterated!");
         break;
 
       default:
